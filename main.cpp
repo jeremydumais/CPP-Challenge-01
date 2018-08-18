31ec2052cb0bd402f77de12a8cc661f371a14af2
jeremydumais@hotmail.com <gIyqyc8PJNDVn0chDDqg>
2018-08-18 15:56:24 -0400

    Challenge completed

--- main.cpp
+++ main.cpp
@@ -7,11 +7,24 @@ int GetInputLimit();
 
 int main(int argc, char const *argv[])
 {
-    int limit {0};
+    unsigned int limit {0};
     cout << "Sommes des nombres divisible par 3 et 5" << endl;
     limit = GetInputLimit();
 
-    cout << "Appuyer sur une toucher pour terminer..." << endl;
+    //Calculation
+    unsigned int total {0};
+    for(unsigned int i=1; i<=limit; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            total += i;
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    cout << "Sum of natural number divisible by either 3 or 5 : " << total << endl;
+
+    cout << "Press any key to continue... " << endl;
     getch();
     return 0;
 }
